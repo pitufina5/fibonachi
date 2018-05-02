6f4bc2c53d3a65b86df1c73d65b13b482218cff5
lola <loladominguezmor@gmail.com>
2018-05-02 13:01:47 +0200

    first commit

---
+++ fibonachi.php
@@ -0,0 +1,12 @@
+<?php
+function fib($n) {
+    if ($n < 0){
+        return NULL;
+    } elseif  ($n == 0) {
+        return 0;
+    } elseif ($n === 1) || $n === 2) {
+        return 1;
+    }  else {  
+        return fib
+
+    }
